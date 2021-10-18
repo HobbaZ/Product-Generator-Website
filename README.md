a57427b094d5896a1e3dd24d0b8986f435760d0d
HobbZ <zachobba@gmail.com>
2021-10-18 16:08:28 +1000

    Fixed generator, reworded some arrays, can now create 5 ideas at a time

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Product-Generator-Website
+This website at the moment allows the user to generate product ideas in the form of a resume experience entry.
+
+### Current version
+![portfolio screenshot](./images/screenshot.png)
+
+### Link
+Go to the [Website link](https://hobbaz.github.io/Product-Generator-Website/)
+
+### How to use
+Wait for the site to generate product ideas then click red button to generate more ideas.
+
+### Issues
+Sometimes the features section will repeat
+
+### Technology Used
+- HTML
+- CSS
+- Javascript
+
+## Future ideas
+- Add generated images
+- Refine colour scheme and layout
+
+### MIT License
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- javascript.js
+++ javascript.js
@@ -1,42 +1,27 @@
 
-var programmingLanguage = ['HTML', 'Python', 'JavaScript', 'COBOL', 'FORTRAN', 'Visual Basic', 'Excel'];
+let programmingLanguage = ['HTML', 'Python', 'JavaScript', 'COBOL', 'FORTRAN', 'Visual Basic', 'Excel', 'Pearl', 'CSS'];
+let secondWord = ['software', 'estate', 'rage', 'report', 'creations', 'stats', 'company','fast', 'rocket', 'velocity', 'arrow', 'express','solutions','ify', 'ly', 'nado', 'graph', 'kit', 'node', 'ooey', 'right', 'hub', 'gin', 'gen', 'en', 'ow', 'or', 'ent', 'ed'];
+let com = ['.uk', '.au', '.gov', ''];
+let position = ['admin', 'support', 'mail', 'tech', 'boss'];
 
-var project = ['procedurally generated', 'AI enabled', 'blockchain encrypted', 'cryptocurrency focused'];
-var interest = ['shoe', 'car', 'real estate', 'drink', 'recipe', 'app', 'game', 'political opinion', 'government'];
-var projectType = ['website', 'app', 'report generator', 'calculator'];
-var action = ['scrolling really fast on your ', 'closing popups tailored to your ', 'watching ads about your ', 'injecting malicious Javascript into your ', 'investing in your ', 'destroying rival competitors to your ', 'spamming your '];
-var task = [ ' can be difficult. ', ' can be a nerve racking venture. ', ' can be an exhausting experience. ', ' can be mundane. ', ' can be daunting. '];
-var accuracy = ['terrible', 'mediocre', 'amazing', 'extraordinary', 'poor', 'surprising'];
-var size = ['insignificant', 'small', 'medium', 'large', 'massive', 'blackhole-esque'];
-var timeSpan = ['the next quantum leap', 'next week', 'yesterday', 'the forfront of tomorrow', 'the latest'];
-var pace = ['break neck speed', 'snail\'s pace', 'spine snapping pace', 'lightspeed'];
-var shareWith = ['the world', 'their parents', 'their friends', 'their collegues', 'the Internet'];
-var secondWord = ['software', 'estate', 'rage', 'report', 'creations', 'stats', 'company','fast', 'rocket', 'velocity', 'arrow', 'express','solutions','ify', 'ly', 'nado', 'graph', 'kit', 'node', 'ooey', 'right', 'hub', 'gin', 'gen', 'en', 'ow', 'or', 'ent', 'ed'];
-var com = ['.uk', '.au', '.hk', '.gov', ''];
-var position = ['admin', 'support', 'mail', 'tech', 'boss'];
+let projects = document.getElementById('projects');
 
-var greeting = ['Web Developer', 'Webtrepreneur', 'Hit me up with your cool web dev projects', 'Please hire me, I\'m desperate', 'Google Savant', 'Dream Creator', ''];
+//General random function
+function GetRandom(arrayInput) {
+    let randNumber = Math.floor(Math.random()*arrayInput.length);
+    return arrayInput[randNumber];
+    }
 
-var aboutmeDesc = [
-    "I'm a full stack web developer based in the Blue Mountains. I use " + GetRandom(programmingLanguage) +
-    " for front end work and " + GetRandom(programmingLanguage)+" for back end work." + "<br>",
+let aboutmeDesc = [
+    "I'm a full stack web developer based in the Blue Mountains. I use a combination of " + GetRandom(programmingLanguage) +
+    "  and " + GetRandom(programmingLanguage)+" to make ideas reality." + "<br>",
 
     "Building powerful applications with just " + GetRandom(programmingLanguage) + " and " + GetRandom(programmingLanguage) +
     " I create the future in the present",
 
-    "Have a cool project idea but don't have the technical skills to get that idea into an app or website? That's where I come in. \
-    My strong foundations in " + GetRandom(programmingLanguage) + " will make me the ideal programmer to tackle your project"
+    "Have a cool project idea but don't have the technical skills to make that idea into an app or website? My strong foundations in " 
+    + GetRandom(programmingLanguage) + " and " + GetRandom(programmingLanguage) + " will make me the ideal programmer to tackle your project"
 ];
-
-//var colour = ['Red', 'LightGreen', 'LightBlue', 'LightYellow', 'LightGrey'];
-
-var arrayInput = [programmingLanguage, project, interest, projectType];
-
-//General random function
-function GetRandom(arrayInput) {
-var randNumber = Math.floor(Math.random()*arrayInput.length);
-return arrayInput[randNumber];
-}
 
 // Capitalizes first letter of each word
 function Capitaliser(input) {
@@ -49,112 +34,104 @@ function Capitaliser(input) {
 
 //strips whitespace from email and converts any uppercase to lowercase
 function emailer(words) {
-    var word = words.split(' ').join('').toLowerCase();
+    let word = words.split(' ').join('').toLowerCase();
     return word;
 }
 
 function projectGenerator() {
-   // var projectsToGen = 5;
-        var chosenProject = GetRandom(project);
-        var chosenInterest = GetRandom(interest);
-        var chosenProjectType = GetRandom(projectType);
 
-        //description of tech use
-        if (chosenProject === project[0]) {
-            var techDescription = "The latest advances in " + chosenProject + " technology allows users to view and collate generated " + chosenInterest + "s on the fly, allowing the user to share their new found " + chosenInterest + " with " + GetRandom(shareWith) + ", or keep it a secret.";
-        } 
+    let project = ['procedurally generated', 'AI enabled', 'blockchain encrypted', 'cryptocurrency focused', 'community created software'];
+    let interest = ['shoe', 'car', 'real estate', 'drink', 'recipe', 'app', 'game', 'political opinion', 'government', 'business', 'plant', 'coffee', 'book'];
+    let projectType = ['website', 'app', 'report generator', 'calculator', "tracker", "finder", "encyclopeadia"];
+    let data = ['collecting', 'milage', 'tracker', 'budgeter', 'renter', 'resale advisor'];
+    let individual = ['busy individual', 'investor', 'driven minimum wage earner', 'CEO', 'entrepreneur', 'retiree'];
+    let descriptor = ['amazing', 'spaghetti coded', 'innovative', 'time-consuming', 'time-saving', 'mundane', 'myriad', 'forgettable']
+    let compSystem = ["Amiga 64", "GameStation", "Orange Operating Systems", "phone only", 'Compubox Computers', 'Partsbox PC', 'überteuert Systems'];
+    let engagement = ['engage', 'bore', 'distract', 'annoy', 'capture']
 
-        if (chosenProject === project[1]) {
-            var techDescription = chosenProject + " technology collects users previous " + chosenInterest + " related data and pulls from various behavioural algorithms and datasets in order to predict the users needs and interests relating to " + chosenInterest +"s, resulting in a " + chosenProjectType + " that can predict users needs with " + GetRandom(accuracy) + " accuracy.";
-        }
+    projects.textContent = "";
 
-        if (chosenProject === project[2]) {
-            var techDescription = "The use of " + chosenProject + " technology propels users at " + GetRandom(pace) + " to " + GetRandom(timeSpan) + " in " + chosenInterest + " based " + chosenProject + " security, productivity and usability. ";
-        }
+    for (let index = 0; index < 5; index++) {
 
-        if (chosenProject === project[3]) {
-            var techDescription = "My " + chosenProject + " " + chosenInterest + " " + chosenProjectType +" is punching a " + GetRandom(size) + " " + chosenInterest + " shaped hole in the traditional computerised passive income generated industry. Generate money by " + GetRandom(action) + chosenInterest + " today. ";
-        }
+    let chosenProject = GetRandom(project);
+    let chosenInterest = GetRandom(interest);
+    let chosenProjectType = GetRandom(projectType);
+    let chosenData = GetRandom(data);
 
-        //method of interest
+    //Generate project name
+    let nameGen = [chosenProject, chosenInterest, chosenProjectType];
+    let randSlice = GetRandom(secondWord).length;
+    //Chooses random name and slices a random amount depending on randSlice number (usually 3 to 6), 
+    //then removes any spaces with replace keyword, then adds a random second word to it
+    let brandName = chosenInterest.slice(0, randSlice).replace(" ","") + GetRandom(secondWord);
 
-        if ((chosenInterest === interest[1]) || (chosenInterest === interest[2])) {
-            var interestDescription =  "The new and used market for " + chosenInterest + "s" + GetRandom(task);
-        }
+    //Gets random name from nameGen, slices it to about half the word plus another random end word, 
+    let projectEmail = "Email: " + GetRandom(position) + "@" + emailer(brandName) + ".com" + GetRandom(com);
 
-        if ((chosenInterest === interest[3]) || (chosenInterest === interest[0])  || (chosenInterest === interest[4]) || (chosenInterest === interest[5]) || (chosenInterest === interest[6])) {
-            var interestDescription =  "Discovering and creating the next hit " + chosenInterest + GetRandom(task);
-        }
-
-        if ((chosenInterest === interest[7]) || (chosenInterest === interest[8])) {
-            var interestDescription =  "Shifting through all the noise around " + chosenInterest + "s and finding the right " + chosenInterest + " for you" + GetRandom(task);
-        }
-
-        //method of project type
-        if (chosenProjectType === projectType[0]) {
-            var projectTypeDescription =  "View the latest info all in one place with my newly developed " + chosenProjectType + " utilising "+ chosenProject + " technology. "; 
-        }
-
-        if (chosenProjectType === projectType[1]) {
-            var projectTypeDescription =  "Have access to " + chosenInterest + "s in the palm of your hand with my newly developed " + chosenProjectType + " utilising "+ chosenProject + " technology. ";
-        }
-
-        if (chosenProjectType === projectType[2]) {
-            var projectTypeDescription =  "Get the stats and engagement you depend upon with my newly developed " + chosenInterest + " " + chosenProject + " " + chosenProjectType + ". Track and customise the report with only the stats you care about, allowing easy to digest information and excluding the information you don't care about. "; 
-        }
-
-        if (chosenProjectType === projectType[3]) {
-            var projectTypeDescription =  "Calculate the everyday costings, efficiency, performance and interactivity among other analytics of your " + chosenInterest + "s with my newly developed " + chosenProject + " " + chosenProjectType + ". ";
-        }
-
-        //Generate product description
-        var description = interestDescription + projectTypeDescription + techDescription;
-
-        //Generate project name
-        var nameGen = [chosenProject, chosenInterest, chosenProjectType];
-        var randSlice = GetRandom(secondWord).length;
-        //Chooses random name and slices a random amount depending on randSlice number (usually 3 to 6), 
-        //then removes any spaces with replace keyword, then adds a random second word to it
-        var brandName = GetRandom(nameGen).slice(0,randSlice).replace(" ","") + GetRandom(secondWord);
-
-        //Gets random name from nameGen, slices it to about half the word plus another random end word, 
-        var projectName = "Name: " + Capitaliser(brandName);
-        var projectEmail = "Email: " + GetRandom(position) + "@" + emailer(brandName) + ".com" + GetRandom(com);
-
-        var title = chosenProject + " " + chosenInterest + " " + chosenProjectType;
-
-        var projectTitle = "<h1>"+ Capitaliser(chosenProjectType) + "</h1>" + "<br>" + Capitaliser(title) + "<br>"
-        + description  + "<br>" + projectName + "<br>" + projectEmail;
-        return projectTitle;
+    let startDescription = [
+        'The latest advances in ' + chosenProject + ' technology',
+        chosenProject + ' technology',
+        'Dumbing down ' + chosenProject + ' technology',
+        'Combining ' + GetRandom(project) + ' technology with trace elements of ' + chosenProject + " algorithms",
+    ];
     
+    let midDescription = [
+        " has enabled us to create " + Capitaliser(brandName) + ", a " + chosenProject + " " + chosenInterest + " " + chosenProjectType + ' that will change the world as we know it. ',
+        " lead us to form " + Capitaliser(brandName) + ', a ' + chosenInterest + " " + chosenData + ' ' + chosenProjectType + ' for the ' + GetRandom(individual)+". ",
+        " created an opportunity in the market for a " + chosenInterest + "  focused " + chosenProjectType + " to fill a much overlooked niche market. "
+    ]
+
+    let endDescription = [
+        Capitaliser(brandName) + " does what other " + chosenProjectType + "'s do, but better, boasting " + GetRandom(descriptor) + " features such as:",
+        "Enhancing users lives with " + GetRandom(descriptor) + " features such as:",
+        Capitaliser(brandName) + " will " + GetRandom(engagement) + " users with such " + GetRandom(descriptor) + " features as:"
+    ]
+
+    let features = [
+    "Easy to navigate features",
+    'Large font for easy reading',
+    'Social media plugins',
+    'A panic button when you make a mistake',
+    'Night mode/day mode',
+    'VR support',
+    'Acheivments to be earned (with money)',
+    'Lootbox mechanics',
+    'Monitor ' + chosenInterest + "'s various personal economic factors with in depth data metrics behind a paywall", 
+    ]
+
+    let list = document.createElement("ul");
+    for (let index = 0; index < 3; index++) {
+        let feature = document.createElement('li');
+        feature.textContent = GetRandom(features);      
+        list.appendChild(feature);
 }
-//First loading of page
 
-//portfolio greeting
-document.getElementById("portfolioHeader").innerHTML='Zac Hobba' + '<br>' + GetRandom(greeting);
+    //Generate product description
+    let projectHeading = document.createElement("h2");
+    projectHeading.textContent = Capitaliser(brandName)
 
-//about me section
+    let description = document.createElement("p");
+    description.textContent = GetRandom(startDescription) + GetRandom(midDescription) + GetRandom(endDescription);
 
+    let subheading = document.createElement("h5");
+    subheading.textContent = 'Software for ' +GetRandom(compSystem);
 
-document.getElementById("aboutme").innerHTML= GetRandom(aboutmeDesc);
+    let email = document.createElement("p");
+    email.textContent = projectEmail;
 
-document.getElementById("projects").innerHTML= projectGenerator();
+    projects.appendChild(projectHeading);
+    projects.appendChild(subheading);
+    projects.appendChild(description);
+    projects.appendChild(list);
+    projects.appendChild(email);
+}
+}
 
 //function to generate new product when clicked
 function generate() {
-    var projects = 5;
-    for (let index = 0; index < projects.length; index++) {
-        projectGenerator();
-    }
-
-    //generate new about me
     document.getElementById("aboutme").innerHTML= GetRandom(aboutmeDesc);
-
-    //Generate header greeting
-    document.getElementById("portfolioHeader").innerHTML='Zac Hobba' + '<br>' + GetRandom(greeting);
-
-    //generate new product
-    document.getElementById("projects").innerHTML= projectGenerator();
-
-    
+    projectGenerator();
 }
+
+document.getElementById("portfolioHeader").innerHTML='Zac Hobba' + '<br>';
+generate();
